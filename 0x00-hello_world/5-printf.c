f163ed57ea600744e432a48b900cce12a10e8e03
Zero64v <kijanavictor96@gmail.com>
2022-09-08 22:57:49 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the function
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        print("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
+^c
