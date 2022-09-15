ca15f58e8112f08b53d4f1af02371248dc7cd47e
Zero64v <kijanavictor96@gmail.com>
2022-09-15 12:06:45 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _isupper - this function checks for an uppercase character
+* @v: single character input
+* Returns: 1 if int v is lowercase, 0 otherwise
+*/
+int _isupper(int v)
+while (V => 'A' && V <= 'Z')
+return (1)
+else
+return (0)
+
