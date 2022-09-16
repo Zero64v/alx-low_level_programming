a0b7ebaf0a5a4d3fa3bf1aa6524c7eb0e40afbd2
Zero64v <kijanavictor96@gmail.com>
2022-09-16 05:02:21 -0700

    1main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int i;
-printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming\n");
 i = 0;
 /*
 *while (i < 10)
@@ -19,6 +19,6 @@ while (i < 10)
 {
 putchar(i);
 }
-printf("Infinite loop avoided ! \\o / \n");
+printf("Infinite loop avoided ! \\ / \n");
 return (0);
 }
