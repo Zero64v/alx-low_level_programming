7fffcbc06da14a5aab94915e70d1efe17d83c729
Zero64v <kijanavictor96@gmail.com>
2022-09-16 00:43:17 -0700

    posnet

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,10 +3,10 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-int main(void);
+int main(void)
 {
 int i;
 i = 0;
-printf("%i is positive\n", n);
+positive_or_negative(i);
 return (0);
 }
