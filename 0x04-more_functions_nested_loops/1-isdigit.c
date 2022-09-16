52f35596559736d2a142cb9621d9a5abf9352d7e
Zero64v <kijanavictor96@gmail.com>
2022-09-16 13:09:23 -0700

    1main

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * _isdigit - function checks for digit 0-9
 * @c - checks function input
 * Return: 1 if int c is digit, otherwise 0
 */
-int _isdigit(void);
 int _isdigit(int c)
 {
-if (c >= 48 && c <= 57)
+if ((c >= 48) && (c <= 57))
+{
 return (1);
-else
-return (0)
 }
-
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @a - first input
+* @b - second input
+* return: product a & b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+int a, b, c;
+c = mul(a * b);
+return (0);
+}
+
