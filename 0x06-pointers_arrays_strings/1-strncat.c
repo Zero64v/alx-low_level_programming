e66b4a15d6c9cb5da8c4b5e893e0aa10f761bfa5
Zero64v <kijanavictor96@gmail.com>
2022-09-22 01:31:14 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
 * @dest: string to be appended upon
 * @src: string to be completed at the end of the dest
 * @n: integer parameter to compare to index
-* Return: return new concatenated string
+*Return: return new concatenated string
 */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -12,6 +12,6 @@ int index = 0, dest_len = 0;
 while (dest[index++])
 dest_len++;
 for (index = 0; src[index] && index < n; index++)
-dest[dest_len] = src[index];
+dest[dest_len++] = src[index];
 return (dest);
 }
