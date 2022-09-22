3de5b7a7735561efae93ea731be59240e235bfb1
Zero64v <kijanavictor96@gmail.com>
2022-09-22 00:00:43 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _strcat - concatenates the string pointed to by @src to
-*the end of the string pointed to by @dest
+* the end of the string pointed to by @dest
 * @dest: String that will be appended
 * @src: String to be concatenated upon
 *
