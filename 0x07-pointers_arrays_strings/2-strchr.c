fc7d21bb603844ca2bb5f48b2514e89473e60f69
Zero64v <kijanavictor96@gmail.com>
2022-09-26 13:22:22 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _strchr - locates a character in a string
+* @s: string to check it
+* @c: character to chech for
+*
+* Return: pointer to spot in s with c or null
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; *(s + 1); i++)
+{
+if (*(s + i) == c)
+return (s + i);
+}
+if (*(s + i) == c)
+return (s + i);
+return (0);
+}
