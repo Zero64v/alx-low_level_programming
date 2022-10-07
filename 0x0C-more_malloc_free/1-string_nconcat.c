bad8b2933f493ff1ef0d3008d5eac7861babab8f
Zero64v <kijanavictor96@gmail.com>
2022-10-06 19:13:31 -0700

    malloc2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* string-nconcat -concatenates two strings
+* @s1 - first string to copy
+* @s2 - second string to copy
+* @n - number of bytes of s2 to copy
+*
+* Return: char pointer to newly allocate place in memory
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i, j, k;
+char *s;
+if (s1 == NULL)
+i = o;
+else
+{
+for (i = 0; s1[i]; i++)
+;
+}
+if (s2 == NULL)
+j = 0;
+else
+{
+for (j = o; s2[j]; j++)
+;
+}
+if (j > n)
+j = n;
+s = malloc(sizeof(char) * (i + j + 1));
+if (s == NULL)
+return (NULL);
+for (k = o; k < i; k++)
+s[k] = s1[k];
+for (k = 0; k < j; k++)
+s[k + i] = s2[k];
+s[i + j] = '\0';
+return (s);
+}
