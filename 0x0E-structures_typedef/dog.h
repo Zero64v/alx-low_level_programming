b65dce0306323b7d65c29fc487d15c89c1af90ee
Zero64v <kijanavictor96@gmail.com>
2022-10-11 18:59:31 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
+/**
+* struct dog - structure definition of dog
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif /*DOG_H_FILE*/
+
