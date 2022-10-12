728899d498c38e0e038edbb0463686faf4e8e695
Zero64v <kijanavictor96@gmail.com>
2022-10-11 19:42:22 -0700

    dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,12 +6,12 @@
 *
 * Return: void
 */
-void free_dog(dog_t *d) 
-{  
-if (d)    
-{      
-free(d->name);      
-free(d->owner);      
-free(d);      
-}  
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
