ba80f80e1bcbed74e9f1fa032374623c102a3577
Zero64veek <veek.circuitry064@gmail.com>
2023-07-19 07:00:48 -0700

    low

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,21 +10,27 @@
  */
 int main(void)
 {
-int n;
-int l;
+  int n;
 
-srand(time(0));
-n = rand() - RAND _ MAX / 2;
-l = n % 10;
-if (1 > 5)
-{
-printf("Last line of %d is %d and is greater than 5\n", n, l);
-}
-else if (1 == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, 1);
-}
-else
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
+  srand(time(0));
+  n = rand() - RAND_MAX/2;
+
+  if((n%10)>5)
+    {
+      printf("Last digit of %d is %d and is greater
+than 5\n",
+	     nn,n % 10);
+    }
+  else if((n % 10)< 6 && (n % 10)! = 0)
+    {
+      printf("Last digit of %d is %d and is less
+than 6 and not 0\n",
+	     n,n % 10);
+    }
+  else
+    {
+      printf("Last digit of %d is %d and is 0\n",
+	     n,n %10);
+    }
+  return (0);
 }
