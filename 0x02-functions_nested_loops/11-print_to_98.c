c725512a624641c79c9da32e40bce4c54ea0aa8f
Zero64veek <veek.circuitry064@gmail.com>
2023-07-19 18:27:10 -0700

    low

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,39 +1,30 @@
+#include<stdio.h>
 #include "main.h"
 /**
 * print_to_98 - a function that prints all natural numbers from n to 98
 * user input's number prints to 98, regardless < 98 or > 98
-* @v: number input
+* @n: number input
 * Return: Always 0 (Success)
 */
-void print_to_98(int v)
+void print_to_98(int n)
 {
-if (v < 98)
-{
-while (v <= 98)
-{
-printf("%d", n);
-if (v != 98)
-{
-printf(", ");
-}
-v++;
-}
-}
-else if (v > 98)
-{
-while (v >= 98)
-{
-printf("%d", n);
-if (v != 98)
-{
-printf(", ");
-}
-v--;
-}
-}
-else
-{
-printf("98");
-}
-printf("\n");
+  int i,j;
+  if(n<=98)
+    {
+      for(i=n;i<=98,i++)
+	{
+	  if(i!=98)
+	    print("%,",i);
+	  else if(i==98)
+	    printf("%d\n",i);
+	}
+    }else if(n>=98)
+    {
+      for(j=n;j>98;j--)
+	{
+	  if(j!=98)\printf("%d,",j);
+	  else if(j==98)
+	    printf("%d\n",j);
+	}
+    }
 }
