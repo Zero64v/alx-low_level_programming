7669fc992edba7edd263ff9bbd7d819c48490933
Zero64veek <veek.circuitry064@gmail.com>
2023-08-07 21:00:35 -0700

    low

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,15 +9,14 @@
 */
 char *create_array(unsigned int size, char c)
 {
-char *array;
-unsigned int index;
-if (size == 0)
-return (NULL);
-array = malloc(sizeof(char) * size);
-if (array == NULL)
-return (NULL);
-for (index = 0; index < size; index++)
-array[index] = c;
-return (array);
-}
+char *str;
+unsigned int i;
 
+str = malloc(sizeof(char) * size);
+if (size == 0 || str == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
+str[i] = c;
+return (str);
+}
