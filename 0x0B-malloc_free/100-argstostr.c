1ace5e7c61a7c79cba167bcfed83fea37ce4c411
Zero64veek <veek.circuitry064@gmail.com>
2023-08-08 20:15:15 -0700

    low

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,21 +8,21 @@
 */
 char *argstostr(int ac, char **av)
 {
-int i, n, r = 0, l = 0;
+int i, n, r = 0, I = 0;
 char *str;
 if (ac == 0 || av == NULL)
 return (NULL);
 for (i = 0; i < ac; i++)
 {
 for (n = 0; av[i][n]; n++)
-l++;
+I++;
 }
-l += ac;
-str = malloc(sizeof(char) * l + 1);
+I += ac;
+str = malloc(sizeof(char) * I + 1);
 if (str == NULL)
 return (NULL);
 for (i = 0; i < ac; i++)
-}
+{
 for (n = 0; av[i][n]; n++)
 {
 str[r] = av[i][n];
